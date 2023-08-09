f6e1476c304da6f278d868e4d2fbf1066ccfc9be
Parsa <parsa82ab@gmail.com>
2023-08-09 19:14:37 +0330

    fixed trending tags

--- pages/views.py
+++ pages/views.py
@@ -7,5 +7,9 @@ from django.db.models import Count
 
 class HomeView(View):
     def get(self, request):
-        tags = Tag.objects.annotate(Count("posts")).order_by("-posts__count")[:3]
+        tags = (
+            Tag.objects.annotate(Count("posts"))
+            .order_by("-posts__count")
+            .order_by("-posts__updated")[:3]
+        )
         return render(request, "pages/home.html", {"tags": tags})
