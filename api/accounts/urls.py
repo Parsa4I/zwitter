bf4d35c36ad30e4ba2bb95ac7fbec798316ba8be
Parsa <parsa82ab@gmail.com>
2023-09-23 20:55:04 +0330

    add password reset api

--- api/accounts/urls.py
+++ api/accounts/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenBlacklistView,
+)
+
+from django_rest_passwordreset.views import (
+    reset_password_confirm,
+    reset_password_request_token,
+    reset_password_validate_token,
 )
 
 
@@ -15,4 +21,15 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
+    path(
+        "password-reset/validate_token/",
+        reset_password_validate_token,
+        name="reset_password_validate",
+    ),
+    path(
+        "password-reset/confirm/", reset_password_confirm, name="reset_password_confirm"
+    ),
+    path(
+        "password-reset/", reset_password_request_token, name="reset_password_request"
+    ),
 ]
--- zwitter/settings.py
+++ zwitter/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+    "django_rest_passwordreset",
 ]
 
 MIDDLEWARE = [
