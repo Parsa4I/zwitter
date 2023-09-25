14af910ceb72f5537dc633e1ec0c16c22eaede35
Parsa <parsa82ab@gmail.com>
2023-09-25 16:00:29 +0330

    update requirements + fix env vars

--- zwitter/settings.py
+++ zwitter/settings.py
@@ -1,7 +1,11 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 from datetime import timedelta
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
